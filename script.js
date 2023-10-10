822d9fc92deff39dc6bd22f8ca6664ab843071ea
Subaiyal-Rehan <123790387+Subaiyal-Rehan@users.noreply.github.com>
2023-10-10 22:53:02 +0500

    Updated

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ let parisInterval;
 let mumbaiInterval;
 let karachiInterval;
 let tokyoInterval;
+let mexicoInterval;
+let chicagoInterval;
+let hongKongInterval;
+let riyadhInterval;
+let moscowInterval;
 
 let activeInterval = null;
 
@@ -44,10 +49,15 @@ function clearIntervals() {
     clearInterval(mumbaiInterval);
     clearInterval(karachiInterval);
     clearInterval(tokyoInterval);
+    clearInterval(mexicoInterval);
+    clearInterval(chicagoInterval);
+    clearInterval(hongKongInterval);
+    clearInterval(riyadhInterval);
+    clearInterval(moscowInterval);
 }
 
 function setActiveButton(buttonId) {
-    const cities = ['LocalTime', 'NewYork', 'London', 'Dubai', 'Paris', 'Mumbai', 'Tokyo', 'Karachi'];
+    const cities = ['LocalTime', 'NewYork', 'London', 'Dubai', 'Paris', 'Mumbai', 'Tokyo', 'Karachi', 'Mexico', 'Chicago', 'HongKong', 'Riyadh'];
     cities.forEach(city => {
         const btn = document.getElementById(city + 'Btn');
         btn.classList.remove("active");
@@ -68,7 +78,7 @@ function LocalTimeClick() {
 
 // New York Time
 function NewYorkClick() {
-    const cities = ['LocalTime', 'Karachi', 'London', 'Dubai', 'Paris', 'Mumbai', 'Tokyo'];
+    const cities = ['LocalTime', 'Karachi', 'London', 'Dubai', 'Paris', 'Mumbai', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== newYorkInterval) {
         clearIntervals();
         const newYork = () => {
@@ -94,7 +104,7 @@ function NewYorkClick() {
 
 // London Time
 function LondonClick() {
-    const cities = ['LocalTime', 'Karachi', 'NewYork', 'Dubai', 'Paris', 'Mumbai', 'Tokyo'];
+    const cities = ['LocalTime', 'Karachi', 'NewYork', 'Dubai', 'Paris', 'Mumbai', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== londonInterval) {
         clearIntervals();
         const london = () => {
@@ -102,7 +112,7 @@ function LondonClick() {
             const londonTime = now.toLocaleString('en-US', { timeZone: 'Europe/London', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
             const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
             const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-    
+
             document.getElementById('time').innerHTML = `${londonTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in London`;
         }
         londonInterval = setInterval(london, 100);
@@ -121,7 +131,7 @@ function LondonClick() {
 
 // Dubai Time
 function DubaiClick() {
-    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Paris', 'Mumbai', 'Tokyo'];
+    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Paris', 'Mumbai', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== dubaiInterval) {
         clearIntervals();
         const dubai = () => {
@@ -129,7 +139,7 @@ function DubaiClick() {
             const dubaiTime = now.toLocaleString('en-US', { timeZone: 'Asia/Dubai', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
             const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
             const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-    
+
             document.getElementById('time').innerHTML = `${dubaiTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Dubai`;
         }
         dubaiInterval = setInterval(dubai, 100);
@@ -148,7 +158,7 @@ function DubaiClick() {
 
 // Paris Time
 function ParisClick() {
-    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Dubai', 'Mumbai', 'Tokyo'];
+    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Dubai', 'Mumbai', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== parisInterval) {
         clearIntervals();
         const paris = () => {
@@ -156,7 +166,7 @@ function ParisClick() {
             const parisTime = now.toLocaleString('en-US', { timeZone: 'Europe/Paris', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
             const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
             const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-    
+
             document.getElementById('time').innerHTML = `${parisTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Paris`;
         }
         parisInterval = setInterval(paris, 100);
@@ -175,7 +185,7 @@ function ParisClick() {
 
 // Mumbai Time
 function MumbaiClick() {
-    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Dubai', 'Paris', 'Tokyo'];
+    const cities = ['LocalTime', 'Karachi', 'NewYork', 'London', 'Dubai', 'Paris', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== mumbaiInterval) {
         clearIntervals();
         const mumbai = () => {
@@ -183,7 +193,7 @@ function MumbaiClick() {
             const mumbaiTime = now.toLocaleString('en-US', { timeZone: 'Asia/Kolkata', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
             const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
             const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-        
+
             document.getElementById('time').innerHTML = `${mumbaiTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Mumbai`;
         }
         mumbaiInterval = setInterval(mumbai, 100);
@@ -202,7 +212,7 @@ function MumbaiClick() {
 
 // Karachi Time
 function KarachiClick() {
-    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Paris', 'Tokyo'];
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Paris', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     const karachiBtn = document.getElementById('KarachiBtn');
     if (activeInterval !== karachiInterval) {
         clearIntervals();
@@ -229,7 +239,7 @@ function KarachiClick() {
 
 // Tokyo Time
 function TokyoClick() {
-    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Paris'];
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Paris', 'Karachi', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
     if (activeInterval !== tokyoInterval) {
         clearIntervals();
         const tokyo = () => {
@@ -247,6 +257,141 @@ function TokyoClick() {
 
     cities.forEach(city => {
         if (city !== 'Tokyo') {
+            const btn = document.getElementById(city + 'Btn');
+            btn.classList.remove("active");
+        }
+    });
+}
+
+
+// Mexico Time
+function MexicoClick() {
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Karachi', 'Tokyo', 'Paris', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
+    if (activeInterval !== mexicoInterval) {
+        clearIntervals();
+        const mexico = () => {
+            const now = new Date();
+            const mexicoTime = now.toLocaleString('en-US', { timeZone: 'America/Mexico_City', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+            const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+
+            document.getElementById('time').innerHTML = `${mexicoTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Mexico`;
+        }
+        mexicoInterval = setInterval(mexico, 100);
+        setActiveButton('MexicoBtn');
+        activeInterval = mexicoInterval;
+    }
+
+    cities.forEach(city => {
+        if (city !== 'Mexico') {
+            const btn = document.getElementById(city + 'Btn');
+            btn.classList.remove("active");
+        }
+    });
+}
+
+
+// Chicago Time
+function ChicagoClick() {
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Karachi', 'Paris', 'Tokyo', 'Mexico', 'HongKong', 'Riyadh', 'Moscow'];
+    const chicagoBtn = document.getElementById('ChicagoBtn');
+    if (activeInterval !== chicagoInterval) {
+        clearIntervals();
+        const chicago = () => {
+            const now = new Date();
+            const chicagoTime = now.toLocaleString('en-US', { timeZone: 'America/Chicago', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+            const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+            document.getElementById('time').innerHTML = `${chicagoTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Chicago`;
+        }
+        chicagoInterval = setInterval(chicago, 100);
+        chicagoBtn.classList.toggle("active", true);
+        activeInterval = chicagoInterval;
+    }
+
+    cities.forEach(city => {
+        if (city !== 'Chicago') {
+            const btn = document.getElementById(city + 'Btn');
+            btn.classList.remove("active");
+        }
+    });
+}
+
+
+// Hong Kong Time
+function HongKongClick() {
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Karachi', 'Paris', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh', 'Moscow'];
+    const hongKongBtn = document.getElementById('HongKongBtn');
+    if (activeInterval !== hongKongInterval) {
+        clearIntervals();
+        const hongKong = () => {
+            const now = new Date();
+            const hongKongTime = now.toLocaleString('en-US', { timeZone: 'Asia/Hong_Kong', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+            const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+            document.getElementById('time').innerHTML = `${hongKongTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Hong Kong`;
+        }
+        hongKongInterval = setInterval(hongKong, 100);
+        hongKongBtn.classList.toggle("active", true);
+        activeInterval = hongKongInterval;
+    }
+
+    cities.forEach(city => {
+        if (city !== 'HongKong') {
+            const btn = document.getElementById(city + 'Btn');
+            btn.classList.remove("active");
+        }
+    });
+}
+
+
+// Riyadh Time
+function RiyadhClick() {
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Karachi', 'Paris', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Moscow'];
+    const riyadhBtn = document.getElementById('RiyadhBtn');
+    if (activeInterval !== riyadhInterval) {
+        clearIntervals();
+        const riyadh = () => {
+            const now = new Date();
+            const riyadhTime = now.toLocaleString('en-US', { timeZone: 'Asia/Riyadh', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+            const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+            document.getElementById('time').innerHTML = `${riyadhTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Riyadh`;
+        }
+        riyadhInterval = setInterval(riyadh, 100);
+        riyadhBtn.classList.toggle("active", true);
+        activeInterval = riyadhInterval;
+    }
+
+    cities.forEach(city => {
+        if (city !== 'Riyadh') {
+            const btn = document.getElementById(city + 'Btn');
+            btn.classList.remove("active");
+        }
+    });
+}
+
+
+// Moscow Time
+function MoscowClick() {
+    const cities = ['LocalTime', 'Mumbai', 'NewYork', 'London', 'Dubai', 'Karachi', 'Paris', 'Tokyo', 'Mexico', 'Chicago', 'HongKong', 'Riyadh'];
+    const moscowBtn = document.getElementById('MoscowBtn');
+    if (activeInterval !== moscowInterval) {
+        clearIntervals();
+        const moscow = () => {
+            const now = new Date();
+            const moscowTime = now.toLocaleString('en-US', { timeZone: 'Europe/Moscow', hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+            const currentDayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+            const currentDate = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
+            document.getElementById('time').innerHTML = `${moscowTime} <br/> on ${currentDayOfWeek}, ${currentDate} <br/> in Moscow`;
+        }
+        moscowInterval = setInterval(moscow, 100);
+        moscowBtn.classList.toggle("active", true);
+        activeInterval = moscowInterval;
+    }
+
+    cities.forEach(city => {
+        if (city !== 'Moscow') {
             const btn = document.getElementById(city + 'Btn');
             btn.classList.remove("active");
         }
